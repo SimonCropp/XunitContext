6d24633e70ee0090926785b5ac32975706fe4941
Simon Cropp <simon.cropp@gmail.com>
2019-12-18 16:05:21 +1100

    Update NonSerializableData.cs

--- src/Tests/Snippets/NonSerializableData.cs
+++ src/Tests/Snippets/NonSerializableData.cs
@@ -1,25 +1,23 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using Xunit;
-using Xunit.Abstractions;
+﻿//using System.Collections.Generic;
+//using System.Security.Claims;
+//using Xunit;
+//using Xunit.Abstractions;
 
-public class NonSerializableData :
-    XunitContextBase
-{
-    [Theory(Skip = "Will throw")]
-    [MemberData(nameof(GetData))]
-    public void Usage(ClaimsPrincipal arg)
-    {
-    }
+//public class NonSerializableData :
+//    XunitContextBase
+//{
+//    [MemberData(nameof(GetData))]
+//    public void Usage(ClaimsPrincipal arg)
+//    {
+//    }
 
-    public static IEnumerable<object[]> GetData()
-    {
-        yield return new object[] {new ClaimsPrincipal()};
-    }
+//    public static IEnumerable<object[]> GetData()
+//    {
+//        yield return new object[] {new ClaimsPrincipal()};
+//    }
 
-    public NonSerializableData(ITestOutputHelper output) :
-        base(output)
-    {
-    }
-}
-
+//    public NonSerializableData(ITestOutputHelper output) :
+//        base(output)
+//    {
+//    }
+//}
