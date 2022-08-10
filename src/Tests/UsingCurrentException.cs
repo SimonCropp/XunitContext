7cf71e0a4c48951db2e39b35d8676452fa41991c
Øyvind Heddeland Instefjord <oyvind.heddeland.instefjord@phonero.no>
2022-08-10 13:20:13 +0200

    Revert "XunitContextBase: Return any captured exception (#336)"
    This reverts commit ebc47b8a95715bbe37001b95d7d84acbaa995ab6.

--- src/Tests/UsingCurrentException.cs
+++ src/Tests/UsingCurrentException.cs
@@ -8,9 +8,6 @@ public class UsingCurrentException :
         Assert.True(false);
 
     [Fact]
-    public void Fails_CommonException() => throw new();
-
-    [Fact]
     public void Passes()
     {
     }
@@ -24,11 +21,11 @@ public class UsingCurrentException :
     {
         var contextTestException = Context.TestException;
         var methodName = Context.Test.TestCase.TestMethod.Method.Name;
-        if (methodName.Contains("Passes"))
+        if (methodName == "Passes")
         {
             Assert.Null(contextTestException);
         }
-        if (methodName.Contains("Fails"))
+        if (methodName == "Fails")
         {
             Assert.NotNull(contextTestException);
         }
