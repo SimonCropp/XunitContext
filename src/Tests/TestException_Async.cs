e913b169411e0f93667725c66d8ac90940d5b90c
Simon Cropp <simon.cropp@gmail.com>
2022-09-14 20:10:35 +1000

    ignore dodgy code

--- src/Tests/TestException_Async.cs
+++ src/Tests/TestException_Async.cs
@@ -1,60 +1,60 @@
-﻿#if DEBUG
-public class TestException_Async:
-    XunitContextBase
-{
-    [Fact]
-    public async Task Root()
-    {
-        await Task.Delay(1);
-        throw new("root");
-    }
-
-    [Fact]
-    public async Task AssertThrows()
-    {
-        await Task.Delay(1);
-        Assert.Throws<Exception>(MethodThatThrows);
-    }
-
-    [Fact]
-    public async Task Caught()
-    {
-        await Task.Delay(1);
-        try
-        {
-            MethodThatThrows();
-        }
-        catch
-        {
-        }
-    }
-
-    [Fact]
-    public async Task FailedAssert()
-    {
-        await Task.Delay(1);
-        Assert.True(false);
-    }
-
-    [Fact]
-    public async Task Nested()
-    {
-        await Task.Delay(1);
-        MethodThatThrows();
-    }
-
-    static void MethodThatThrows() =>
-        throw new("nested");
-
-    public TestException_Async(ITestOutputHelper output) :
-        base(output)
-    {
-    }
-
-    public override void Dispose()
-    {
-        var theExceptionThrownByTest = Context.TestException;
-        base.Dispose();
-    }
-}
-#endif
+﻿// #if DEBUG
+// public class TestException_Async:
+//     XunitContextBase
+// {
+//     [Fact]
+//     public async Task Root()
+//     {
+//         await Task.Delay(1);
+//         throw new("root");
+//     }
+//
+//     [Fact]
+//     public async Task AssertThrows()
+//     {
+//         await Task.Delay(1);
+//         Assert.Throws<Exception>(MethodThatThrows);
+//     }
+//
+//     [Fact]
+//     public async Task Caught()
+//     {
+//         await Task.Delay(1);
+//         try
+//         {
+//             MethodThatThrows();
+//         }
+//         catch
+//         {
+//         }
+//     }
+//
+//     [Fact]
+//     public async Task FailedAssert()
+//     {
+//         await Task.Delay(1);
+//         Assert.True(false);
+//     }
+//
+//     [Fact]
+//     public async Task Nested()
+//     {
+//         await Task.Delay(1);
+//         MethodThatThrows();
+//     }
+//
+//     static void MethodThatThrows() =>
+//         throw new("nested");
+//
+//     public TestException_Async(ITestOutputHelper output) :
+//         base(output)
+//     {
+//     }
+//
+//     public override void Dispose()
+//     {
+//         var theExceptionThrownByTest = Context.TestException;
+//         base.Dispose();
+//     }
+// }
+// #endif
--- src/Tests/TestException_Sync.cs
+++ src/Tests/TestException_Sync.cs
@@ -1,47 +1,47 @@
-﻿#if DEBUG
-public class TestException_Sync :
-    XunitContextBase
-{
-    [Fact]
-    public void Root() =>
-        throw new("root");
-
-    [Fact]
-    public void AssertThrows() =>
-        Assert.Throws<Exception>(MethodThatThrows);
-
-    [Fact]
-    public void Caught()
-    {
-        try
-        {
-            MethodThatThrows();
-        }
-        catch
-        {
-        }
-    }
-
-    [Fact]
-    public void FailedAssert() =>
-        Assert.True(false);
-
-    [Fact]
-    public void Nested() =>
-        MethodThatThrows();
-
-    static void MethodThatThrows() =>
-        throw new("nested");
-
-    public TestException_Sync(ITestOutputHelper output) :
-        base(output)
-    {
-    }
-
-    public override void Dispose()
-    {
-        var theExceptionThrownByTest = Context.TestException;
-        base.Dispose();
-    }
-}
-#endif
+﻿// #if DEBUG
+// public class TestException_Sync :
+//     XunitContextBase
+// {
+//     [Fact]
+//     public void Root() =>
+//         throw new("root");
+//
+//     [Fact]
+//     public void AssertThrows() =>
+//         Assert.Throws<Exception>(MethodThatThrows);
+//
+//     [Fact]
+//     public void Caught()
+//     {
+//         try
+//         {
+//             MethodThatThrows();
+//         }
+//         catch
+//         {
+//         }
+//     }
+//
+//     [Fact]
+//     public void FailedAssert() =>
+//         Assert.True(false);
+//
+//     [Fact]
+//     public void Nested() =>
+//         MethodThatThrows();
+//
+//     static void MethodThatThrows() =>
+//         throw new("nested");
+//
+//     public TestException_Sync(ITestOutputHelper output) :
+//         base(output)
+//     {
+//     }
+//
+//     public override void Dispose()
+//     {
+//         var theExceptionThrownByTest = Context.TestException;
+//         base.Dispose();
+//     }
+// }
+// #endif
