8262b6edc1ef9f99f2dff1e3ccdf38d00ab4ba74
Simon Cropp <simon.cropp@gmail.com>
2023-09-06 22:06:07 +1000

    Update Context_Parameters.cs

--- src/XunitContext/Context_Parameters.cs
+++ src/XunitContext/Context_Parameters.cs
@@ -47,8 +47,10 @@ public partial class Context
     #endregion
 
     static Exception NewNoArgumentsDetectedException() =>
-        new(@"No arguments detected for method with parameters.
-This is most likely caused by using a parameter that Xunit cannot serialize.
-Instead pass in a simple type as a parameter and construct the complex object inside the test.
-Alternatively; override the current parameters using `UseParameters()` via the current test base class, or via `XunitContext.Current.UseParameters()`.");
+        new("""
+            No arguments detected for method with parameters.
+            This is most likely caused by using a parameter that Xunit cannot serialize.
+            Instead pass in a simple type as a parameter and construct the complex object inside the test.
+            Alternatively; override the current parameters using `UseParameters()` via the current test base class, or via `XunitContext.Current.UseParameters()`.
+            """);
 }
